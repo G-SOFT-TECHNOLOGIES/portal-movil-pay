b8aabba6f3dc1fb35900a2f74dee26befd82e224
cleidygil <cleidygil42@gmail.com>
2023-12-08 15:20:58 -0400

    UPDATE creando cupones

--- src/app/components/usuario/components/dialogo-resumen-cupones/dialogo-resumen-cupones.component.ts
+++ src/app/components/usuario/components/dialogo-resumen-cupones/dialogo-resumen-cupones.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { UsuarioService } from '../../services/usuario.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { SnackbarService } from 'src/app/components/service/snackbar.service';
 
 @Component({
   selector: 'app-dialogo-resumen-cupones',
@@ -12,7 +13,7 @@ export class DialogoResumenCuponesComponent {
   private usuario = inject(UsuarioService)
   private router = inject(Router)
   private rout = inject(ActivatedRoute)
-
+  private snack = inject(SnackbarService)
   paquetes = this.usuario.campaings$
   totalAmount = this.usuario.totalAcumulado$
   misCupones: number = this.usuario.mispuntos.value
@@ -36,10 +37,26 @@ export class DialogoResumenCuponesComponent {
     this.router.navigate(['home/servicios_tv/planes/finalizar', this.id])
   }
   canjearCupon() {
+    let arr: any[] = []
+    this.paquetes.subscribe(data => {
+      data.map((cupons) => {
+        console.log(cupons, 'arr')
+        arr.push(cupons.id)
+      })
+    })
     let body = {
-      'campaign_coupon': this.paquetes,
+      'campaign_coupon': arr,
       'contract': this.id,
     }
+    console.log(body, 'el body')
+    this.usuario.setGenerateCuopon(body).then((result) => {
+      console.log(result)
+      this.snack.openSnack("Cupon canjeado correctamente", '')
+    }).catch((error) => {
+      console.log(error)
+      this.snack.openSnack("Eror al canjear cupon", '')
+
+    })
   }
   ngOnDestroy(): void {
     this.sub.unsubscribe()
