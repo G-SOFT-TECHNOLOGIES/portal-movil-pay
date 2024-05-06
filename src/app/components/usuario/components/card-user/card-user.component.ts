8c2ee3228dd047993dd74954a7c116b3e61521b7
Cleidy Gil <cleidygil42@gmail.com>
2024-05-06 18:00:33 -0400

    MOD Validacion de fechas y nodo

--- src/app/components/usuario/components/card-user/card-user.component.ts
+++ src/app/components/usuario/components/card-user/card-user.component.ts
@@ -1,15 +1,14 @@
 import { Component, Input, inject } from '@angular/core';
 import { ConfirmService } from 'src/app/components/service/confirm.service';
 import { UsuarioService } from '../../services/usuario.service';
-import { Contract } from '../../interfaces/contractosInterfaces';
 import { Router } from '@angular/router';
 import { ServicoTvService } from 'src/app/components/servicios/services/servico-tv.service';
-import { ContractDetail } from 'src/app/components/ajustes/interfaces/TicketInterfaces';
 import { SnackbarService } from 'src/app/components/service/snackbar.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogFirmaComponent } from '../dialog-firma/dialog-firma.component';
 import { FacturaContratoService } from 'src/app/components/finanzas/services/usuario.service';
 import { LoginService } from 'src/app/components/auth/services/login.service';
+import { ContractDetail, ContratoID } from 'src/app/components/finanzas/interfaces/UsuarioIDInterface';
 
 @Component({
   selector: 'app-card-user',
@@ -17,7 +16,7 @@ import { LoginService } from 'src/app/components/auth/services/login.service';
   styleUrls: ['./card-user.component.css']
 })
 export class CardUserComponent {
-  @Input() contratos: Contract | any
+  @Input() contratos: ContratoID | any
   private router = inject(Router)
   private tvservices = inject(ServicoTvService);
   private snack = inject(SnackbarService);
@@ -26,15 +25,16 @@ export class CardUserComponent {
   private fuser = inject(FacturaContratoService)
   private login = inject(LoginService)
   gtv: any
+  permiso: boolean | any
   constructor() {
     this.gtv = this.login.contratos$.value.filter((menu) => menu.code == "gtv_contrato").map(data => data)
   }
-  contract: ContractDetail | any
+  contract: ContractDetail[] = []
   date = new Date()
   ngOnInit(): void {
-
     this.tvservices.getContratoTV(this.contratos.id).then((value) => {
       this.contract = value.contract_detail
+      this.permiso = this.permisosFechaZona()
     }).catch((error) => {
       console.log(error)
 
@@ -44,7 +44,7 @@ export class CardUserComponent {
     // console.log(this.contratos.zone, 'date', this.date.getDate(), this.date.getMonth()+1)
     // Carayaca 
     let option: boolean = false
-    let nodo = this.contratos.contract_detail.filter((cont:ContractDetail) => cont.service_type.id == 1).map((cont:ContractDetail) => cont.nodo)
+    let nodo = this.contract.filter((cont) => cont.service_type.id == 1).map(cont => cont.nodo)
     if (this.date.getDate() >= 1 && this.date.getMonth() + 1 == 5 && this.contratos.parish == 5) {
       option = true
     }
@@ -53,20 +53,20 @@ export class CardUserComponent {
       option = true
     }
     // Este
-    if (this.date.getDay() >= 6 && this.date.getMonth() + 1 == 5 && nodo[0] == 9 && nodo[0] == 14 && nodo[0] == 15 && nodo[0] == 16 && nodo[0] == 18 && nodo[0] == 19 && nodo[0] == 22) {
+    if (this.date.getDate() >= 6 && this.date.getMonth() + 1 == 5 && nodo[0] == 9 || nodo[0] == 14 || nodo[0] == 15 || nodo[0] == 16 || nodo[0] == 18 || nodo[0] == 19 || nodo[0] == 22) {
       option = true
     }
     // Centro
-    if (this.date.getDate() >= 13 && this.date.getMonth() + 1 == 5 && this.contratos.zone == 2) {
+    if (this.date.getDate() >=13 && this.date.getMonth() + 1 == 5 && this.contratos.zone == 2) {
       option = true
     }
     // Oeste
-    if (this.date.getDay() >= 20 && this.date.getMonth() + 1 == 5 && this.contratos.zone == 1) {
+    if (this.date.getDate() >= 20 && this.date.getMonth() + 1 == 5 && this.contratos.zone == 1) {
       option = true
     }
     return option
   }
-  redireccionar(contrato: Contract) {
+  redireccionar(contrato: ContratoID) {
     const id = contrato.id
     this.router.navigate([`contratos/finanzas`])
   }
