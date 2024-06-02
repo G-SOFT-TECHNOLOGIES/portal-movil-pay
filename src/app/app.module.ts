6147a610fc2990b97995c51379fb65c63cbdc66d
Reiwil Lugo <reiwillugo@gmail.com>
2024-06-02 14:20:43 -0400

    Change date format to DD-MM-YYYY

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,28 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { PrimengModule } from './components/primeng/primeng.module';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MaterialExampleModule } from 'src/material.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { UsuarioComponent } from './components/usuario/usuario.component';
-import { ServiciosComponent } from './components/servicios/servicios.component';
-import { TicketsComponent } from './components/tickets/tickets.component';
-import { FinanzasComponent } from './components/finanzas/finanzas.component';
-import { AjustesComponent } from './components/ajustes/ajustes.component';
-import { HeadersInterceptor } from './components/helper/headers.interceptor';
-import { TokenInvalidInterceptor } from './components/helper/token-invalid.interceptor';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+// Modules
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialExampleModule } from 'src/material.module';
+import { PrimengModule } from './components/primeng/primeng.module';
+// Components
+import { AppComponent } from './app.component';
 import { DialogConfirmComponent } from './components/components/dialog-confirm/dialog-confirm.component';
 import { DialogoActualizacionesComponent } from './components/components/dialogo-actualizaciones/dialogo-actualizaciones.component';
 import { DialogoPasosAfiliacionComponent } from './components/components/dialogo-pasos-afiliacion/dialogo-pasos-afiliacion.component';
 import { DialogoAlertasGlobalesComponent } from './components/components/dialogo-alertas-globales/dialogo-alertas-globales.component';
-import { GtvComponent } from './components/gtv/gtv.component';
 import { DialogoGtvPasosComponent } from './components/components/dialogo-gtv-pasos/dialogo-gtv-pasos.component';
+// Interceptors
+import { TokenInvalidInterceptor } from './components/helper/token-invalid.interceptor';
+import { HeadersInterceptor } from './components/helper/headers.interceptor';
+// Utils
+import { MY_FORMATS, locale } from './components/utils/DateFormater.util';
 
 
 @NgModule({
@@ -59,6 +59,12 @@ import { DialogoGtvPasosComponent } from './components/components/dialogo-gtv-pa
       useClass: TokenInvalidInterceptor,
       multi: true,
     },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter
+    },
+    { provide: MAT_DATE_LOCALE, useValue: locale },
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS }
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
